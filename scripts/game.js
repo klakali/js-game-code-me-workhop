1cc461e544e8a5ecc5eaa1684e9a839bb00b13c5
KlaKali1 <klaudia.m.kalinowska@gmail.com>
2018-06-17 19:36:25 +0200

    new new

--- scripts/game.js
+++ scripts/game.js
@@ -6,38 +6,39 @@
     const panelElement = document.createElement("div");
     const arenaElement = document.createElement("div");
     const moleElement = document.createElement("div");
-    //panelElement.innerHTML = score;
-
+    const scoreElement = document.createElement("div");
+    const resetButtonElement = document.createElement("a");
+        resetButtonElement.innerHTML = "Reset";
+        resetButtonElement.setAttribute("role", "button");
+        resetButtonElement.setAttribute("href", "#reset");
+    
     gameElement.classList.add("game");
     panelElement.classList.add("panel");
     arenaElement.classList.add("arena");
     moleElement.classList.add("mole");
-
+    
     gameElement.appendChild(panelElement);
     gameElement.insertAdjacentElement(
         "beforeEnd",
         arenaElement
     );
-
-    const resetButtonElement = document.createElement("a");
-    resetButtonElement.innerHTML = "reset";
-    resetButtonElement.setAttribute("role", "button");
-    resetButtonElement.setAttribute("href", "http://codeme.pl");
+    
+    scoreElement.innerHTML = score;
+    
+    panelElement.appendChild(scoreElement);
+    panelElement.appendChild(resetButtonElement);;
+    arenaElement.appendChild(moleElement);
+    
+    document.body.appendChild(gameElement);
 
     resetButtonElement.addEventListener("click", function (e) {
         e.stopPropagation();
         e.preventDefault();
 
         score = 0;
+        scoreElement.innerHTML = score;
         showMole();
     }, false);
-
-    const scoreElement = document.createElement("div");
-    scoreElement.innerHTML = score;
-
-    panelElement.appendChild(resetButtonElement);
-    panelElement.appendChild(scoreElement);
-    arenaElement.appendChild(moleElement);
 
     arenaElement.addEventListener(
         "click",
@@ -56,8 +57,6 @@
 
     showMole();
 
-    document.body.appendChild(gameElement);
-
     function changePosition() {
         Object.assign(
             moleElement.style, {
@@ -70,38 +69,6 @@
     function showMole() {
         clearInterval(timeUID);
         changePosition()
-        timeUID = setInterval(changePosition, 1 * 30 * 1000);
+        timeUID = setInterval(changePosition, 1 * 9 * 100);
     }
-}());
-
-(function () {
-    const html = `<div class="game" ref="game">
-        <div class="panel" ref="panel">
-            <a role="button" href="#reset" ref="resetButton">reset</a>
-            <div ref="score">0</div>
-        </div>
-        <div class="arena"  ref="arena">
-            <div class="mole" ref="mole"></div>
-        </div>
-    </div>`;
-
-    const tmp = document.createElement("template");
-
-    tmp.innerHTML = html;
-
-    const documentFragment = tmp.content;
-
-    const colection = documentFragment.querySelectorAll("[ref]");
-
-    const template = Array.prototype.reduce.call(
-        colection,
-        function (obj, item) {
-            const name = item.getAttribute("ref");
-            obj[name] = item;
-            return obj;
-        }, {});
-
-    template.arena.addEventListener("click", function () {
-
-    }, false);
 }());
